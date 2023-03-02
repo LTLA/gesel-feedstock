2e8a8eb1651876a1bb463d63ab2dc69c55f7a2bf
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-03-01 22:56:08 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Build gene sets to feed gesel
+
+Prepares GMT files to feed into **gesel**, the client-side gene set search interface.
+All GMT files are expected to use Ensembl identifiers.
---
+++ go/build.R
@@ -0,0 +1,43 @@
+GO.from.org <- function(orgdb, prefix) {
+    mappings <- select(orgdb, keytype="GO", keys=keys(orgdb, "GO"), columns="ENSEMBL")
+
+    library(BiocParallel)
+    output <- split(mappings$ENSEMBL, mappings$GO)
+    output <- bplapply(output, function(x) {
+        current <- unique(sort(x))
+        paste(current, collapse="\t")
+    }, BPPARAM=MulticoreParam())
+
+    # Saving the names and descriptions.
+    library(GO.db)
+    info <- select(GO.db, keys=names(output), column="TERM")
+    payload <- sprintf("%s\t%s\t%s", names(output), info$TERM[match(names(output), info$GOID)], output)
+
+    con <- gzfile(paste0(prefix, ".gmt.gz"), open="wb")
+    write(payload, file=con)
+    close(con)
+
+    # Emitting relevant session information.
+    print(metadata(orgdb))
+}
+
+library(org.Hs.eg.db)
+GO.from.org(org.Hs.eg.db, "human-GO")
+
+library(org.Mm.eg.db)
+GO.from.org(org.Mm.eg.db, "mouse-GO")
+
+library(org.Dm.eg.db)
+GO.from.org(org.Dm.eg.db, "fly-GO")
+
+library(org.Ce.eg.db)
+GO.from.org(org.Ce.eg.db, "worm-GO")
+
+library(org.Rn.eg.db)
+GO.from.org(org.Rn.eg.db, "rat-GO")
+
+library(org.Pt.eg.db)
+GO.from.org(org.Pt.eg.db, "chimp-GO")
+
+library(org.Dr.eg.db)
+GO.from.org(org.Dr.eg.db, "zebrafish-GO")
