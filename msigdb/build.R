0aaf4483dfe8b10279763bc6fe40c688d8e141c1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-03-11 17:08:17 -0800

    Simplified the build scripts.

--- msigdb/build.R
+++ msigdb/build.R
@@ -1,9 +1,12 @@
-# Need to prune out gene sets with difficult licensing terms.
+# Need to prune out gene sets with difficult licensing terms
+# or are redundant with each other.
 
-version <- "v2022.1"
+version <- "v2023.1"
 ignore <- c(
     "c2.cp.kegg", # difficult license
+    "m2.cp.kegg", # difficult license
     "c2.cp.biocarta", # difficult license
+    "m2.cp.biocarta", # difficult license
     "c2.all", # includes the above
     paste0("c2.cp.", version), # includes the above
     "c3.all", # too much stuff
@@ -14,26 +17,21 @@ ignore <- c(
     "msigdb" # too much stuff, and also includes the difficult licenses.
 )
 
-library(org.Hs.eg.db)
-mapping <- mapIds(org.Hs.eg.db, keys=keys(org.Hs.eg.db), keytype="ENTREZID", column="ENSEMBL")
-
-dir.create("output")
-dir <- sprintf("msigdb_%s.Hs_files_to_download_locally/msigdb_%s.Hs_GMTs", version, version)
-
-for (x in list.files(dir, pattern="\\.entrez\\.gmt$")) {
-    if (any(startsWith(x, ignore))) {
-        next 
+zip_and_dump <- function(indir, outdir) {
+    dir.create(outdir, showWarnings=FALSE)
+    for (x in list.files(indir, pattern="\\.entrez\\.gmt$")) {
+        if (any(startsWith(x, ignore))) {
+            next 
+        }
+        contents <- readLines(file.path(indir, x))
+        handle <- gzfile(file.path(outdir, paste0(x, ".gz")), open="wb")
+        writeLines(contents, con=handle)
+        close(handle)
     }
-
-    contents <- strsplit(readLines(file.path(dir, x)), "\t")
-    output <- character(length(contents))
-    for (i in seq_along(contents)) {
-        current <- contents[[i]]
-        ids <- mapping[tail(current, -2)]
-        output[i] <- paste(c(head(current, 2), ids[!is.na(ids)]), collapse="\t")
-    }
-
-    handle <- gzfile(file.path("output", paste0(x, ".gz")), open="wb")
-    write(output, file=handle)
-    close(handle)
 }
+
+dir <- sprintf("msigdb_%s.Hs_files_to_download_locally/msigdb_%s.Hs_GMTs", version, version)
+zip_and_dump(dir, "human_output")
+
+dir <- sprintf("msigdb_%s.Mm_files_to_download_locally/msigdb_%s.Mm_GMTs", version, version)
+zip_and_dump(dir, "mouse_output")
